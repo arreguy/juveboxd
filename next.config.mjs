62ec460c441ce28b07e7fc0f87d8e70021335e13
Breno Pandino <brenoarreguy@gmail.com>
2025-10-31 18:54:04 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+const isProjectPage = true;           // set to false if deploying to user/org site (user.github.io)
+const repo = 'juveboxd';             // <-- change to your repository name
 
-export default nextConfig
+export default {
+  output: 'export',                   // static export
+  images: { unoptimized: true },      // Next/Image works on GH Pages
+  basePath: isProjectPage ? `/${repo}` : undefined,
+  assetPrefix: isProjectPage ? `/${repo}/` : undefined,
+  trailingSlash: true,                // avoids some GH Pages mime/redirect quirks
+};
